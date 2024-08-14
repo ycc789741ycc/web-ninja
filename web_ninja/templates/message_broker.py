21144ff06363c1036e9d31c296396eb06b64bf20
Yoshi Gao <yoshi4868686@gmail.com>
2024-08-14 23:26:44 +0800

    chore: Fix directory.

--- web_ninja/templates/message_broker.py
+++ web_ninja/templates/message_broker.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from web_ninja.core.template.module import ModuleTemplate
+from web_ninja.core.template.module import ModuleTemplate, ModuleFile
 
 
 
@@ -9,23 +9,23 @@ class MessageBrokerTemplate(ModuleTemplate):
         self.child_dirs = [Path("message_broker")]
 
         message_broker_base = ""
-        with open(Path("web_ninja/templates/message_broker.py"), "r") as f:
+        with open(Path("web_ninja/modules/message_broker/base.py"), "r") as f:
             message_broker_base = f.read()
         message_broker_celery = ""
         with open(Path("web_ninja/modules/message_broker/celery.py"), "r") as f:
             message_broker_celery = f.read()
 
         self.module_files = [
-            {
-                "child_path": Path("message_broker/__init__.py"),
-                "content": "",
-            },
-            {
-                "child_path": Path("message_broker/base.py"),
-                "content": message_broker_base,
-            },
-            {
-                "child_path": Path("message_broker/celery.py"),
-                "content": message_broker_celery,
-            }
+            ModuleFile(
+                child_path=Path("message_broker/__init__.py"),
+                content=""
+            ),
+            ModuleFile(
+                child_path=Path("message_broker/base.py"),
+                content=message_broker_base,
+            ),
+            ModuleFile(
+                child_path=Path("message_broker/celery.py"),
+                content=message_broker_celery,
+            )
         ]
